b1ecd08cf21894fc4090e37d27d8ed682850a676
Alan Tsarakov <alantsarakov@yandex.ru>
2021-05-21 16:51:37 +0300

    доработки

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,29 @@
 let popup = document.querySelector('.popup');
 let openPopup = document.querySelector('.info__edit');
 let closePopup = document.querySelector('.popup__close');
+let nameInput = document.querySelector('.popup__name');
+let aboutPopup = document.querySelector('.popup__about');
+let formPopup = document.querySelector('.popup__container')
+let nameInfo = document.querySelector('.info__name');
+let aboutInfo = document.querySelector('.info__about');
+let savePopup = document.querySelector('.popup__submit')
 
-openPopup.addEventListener('click', toggleClass);
-
-closePopup.addEventListener('click', toggleClass);
-
-function toggleClass() {
+function togglePopupClass() {
   popup.classList.toggle('popup_opened');
 }
+
+openPopup.addEventListener('click', togglePopupClass);
+closePopup.addEventListener('click', togglePopupClass);
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  nameInput.value = nameInfo.textContent;
+  aboutPopup.value = aboutInfo.textContent;
+}
+
+formPopup.addEventListener('submit', formSubmitHandler);
+
+
 
 popup.addEventListener('click', function(event) {
   if(event.target === event.currentTarget) {

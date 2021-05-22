05aba2c7aad8cacbef6b7d3a19e8a2463f61f13d
Alan Tsarakov <alantsarakov@yandex.ru>
2021-05-23 00:36:31 +0300

    feature: доработать js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 let popup = document.querySelector('.popup');
-let openPopup = document.querySelector('.info__edit');
+let openPopup = document.querySelector('.info__edit-button');
 let closePopup = document.querySelector('.popup__close');
 let nameInput = document.querySelector('.popup__name');
 let aboutPopup = document.querySelector('.popup__about');
-let formPopup = document.querySelector('.popup__container')
+let formPopup = document.querySelector('.popup__form');
 let nameInfo = document.querySelector('.info__name');
 let aboutInfo = document.querySelector('.info__about');
-let savePopup = document.querySelector('.popup__submit')
+let savePopup = document.querySelector('.popup__submit-button')
 
 function togglePopupClass() {
   popup.classList.toggle('popup_opened');
@@ -15,18 +15,23 @@ function togglePopupClass() {
 openPopup.addEventListener('click', togglePopupClass);
 closePopup.addEventListener('click', togglePopupClass);
 
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-  nameInput.value = nameInfo.textContent;
+nameInput.value = nameInfo.textContent;
   aboutPopup.value = aboutInfo.textContent;
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  nameInfo.textContent = nameInput.value;
+  aboutInfo.textContent = aboutPopup.value;
+  togglePopupClass();
 }
 
 formPopup.addEventListener('submit', formSubmitHandler);
 
 
 
+
 popup.addEventListener('click', function(event) {
   if(event.target === event.currentTarget) {
-    toggleClass();
+    togglePopupClass();
   }
 })

4f03384fb3826b928c04e3f23ff37beb9a29f73c
Alan Tsarakov <alantsarakov@yandex.ru>
2021-05-30 22:04:01 +0300

    feat: доработать js

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,39 @@
-let popup = document.querySelector('.popup');
-let openPopup = document.querySelector('.info__edit-button');
-let closePopup = document.querySelector('.popup__close');
-let nameInput = document.querySelector('.popup__input_type_name');
-let aboutPopup = document.querySelector('.popup__input_type_about');
-let formPopup = document.querySelector('.popup__form');
-let nameInfo = document.querySelector('.info__name');
-let aboutInfo = document.querySelector('.info__about');
-  
-function addPopupClass() {
-  popup.classList.add('popup_opened');
-  nameInput.value = nameInfo.textContent;
-  aboutPopup.value = aboutInfo.textContent;
-}
+const editPopup = document.querySelector('.popup_type_edit');
+const openEditPopup = document.querySelector('.info__edit-button');
+const closePopup = document.querySelector('.popup__close');
+const nameEditInput = document.querySelector('.popup__input_type_name');
+const aboutEditInput = document.querySelector('.popup__input_type_about');
+const formEditPopup = document.querySelector('.popup__form');
+const nameInfo = document.querySelector('.info__name');
+const aboutInfo = document.querySelector('.info__about');
 
-function removePopupClass() {
-  popup.classList.remove('popup_opened');
-}
+const likeButton = document.querySelector('.element__like');
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  nameInfo.textContent = nameInput.value;
-  aboutInfo.textContent = aboutPopup.value;
-  removePopupClass();
+  nameInfo.textContent = nameEditInput.value;
+  aboutInfo.textContent = aboutEditInput.value;
+  removeOpenPopupClass(editPopup);
 }
 
-openPopup.addEventListener('click', addPopupClass);
-closePopup.addEventListener('click', removePopupClass);
+function addOpenPopupClass(popup) {
+  popup.classList.add('popup_opened');
+};
+function removeOpenPopupClass(popup) {
+  popup.classList.remove('popup_opened');
+};
 
-formPopup.addEventListener('submit', formSubmitHandler);
+openEditPopup.addEventListener('click', addOpenPopupClass(editPopup));
+closePopup.addEventListener('click', removeOpenPopupClass(editPopup));
+formEditPopup.addEventListener('submit', formSubmitHandler);
+
+likeButton.addEventListener('click', (evt) => {
+  evt.target.classList.toggle('element__like_active');
+});
 
 
+//nameEditInput.value = nameInfo.textContent;
+//aboutEditInput.value = aboutInfo.textContent;
 
 /* с этим пока не разобрался
 popup.addEventListener('click', function(event) {

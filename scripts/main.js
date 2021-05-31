24b5b6df8cc0b8bd45a7b04656ad5431d3de842f
Alan Tsarakov <alantsarakov@yandex.ru>
2021-05-31 16:47:18 +0300

    feat: доработать js2

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+//Элементы Profile
 const editPopup = document.querySelector('.popup_type_edit');
 const openEditPopup = document.querySelector('.info__edit-button');
 const closePopup = document.querySelector('.popup__close');
@@ -7,8 +34,32 @@ const formEditPopup = document.querySelector('.popup__form');
 const nameInfo = document.querySelector('.info__name');
 const aboutInfo = document.querySelector('.info__about');
 
-const likeButton = document.querySelector('.element__like');
+//Элементы карточек
+const templateElement = document.querySelector('.template-element').content;
+const contentBlockElements = document.querySelector('.page__elements');
 
+//Функция создания карточки
+function renderElement(name, link) {
+  const contentElement = templateElement.cloneNode(true);
+  const elementText = contentElement.querySelector('.element__text');
+  const elementPhoto = contentElement.querySelector('.element__img');
+  const deleteElementButton = contentElement.querySelector('.element__delete-button');
+  const likeButton = contentElement.querySelector('.element__like-button');
+
+  elementText.textContent = name;
+  elementPhoto.alt = name;
+  elementPhoto.src = link;
+  contentBlockElements.prepend(contentElement);
+  return contentElement;
+}
+
+function renderElements() {
+  initialCards.forEach(renderElement);
+}
+
+renderElements();
+
+//Функция отправки формы Профиля
 function formSubmitHandler(evt) {
   evt.preventDefault();
   nameInfo.textContent = nameEditInput.value;
@@ -16,24 +67,37 @@ function formSubmitHandler(evt) {
   removeOpenPopupClass(editPopup);
 }
 
+//Функция открытия попапов
 function addOpenPopupClass(popup) {
   popup.classList.add('popup_opened');
-};
+}
+//Функция закрытия попапов
 function removeOpenPopupClass(popup) {
   popup.classList.remove('popup_opened');
-};
+}
 
-openEditPopup.addEventListener('click', addOpenPopupClass(editPopup));
-closePopup.addEventListener('click', removeOpenPopupClass(editPopup));
+
+
+//Открыть попап редактирования Профиля
+openEditPopup.addEventListener('click', () => {
+  addOpenPopupClass(editPopup);
+  nameEditInput.value = nameInfo.textContent;
+  aboutEditInput.value = aboutInfo.textContent;
+});
+//Закрыть попап редактирования Профиля
+closePopup.addEventListener('click', () => {
+  removeOpenPopupClass(editPopup);
+});
+//Отправка формы попапа редактирования Профиля
 formEditPopup.addEventListener('submit', formSubmitHandler);
 
+//Поставить/убрать like
 likeButton.addEventListener('click', (evt) => {
   evt.target.classList.toggle('element__like_active');
 });
 
 
-//nameEditInput.value = nameInfo.textContent;
-//aboutEditInput.value = aboutInfo.textContent;
+
 
 /* с этим пока не разобрался
 popup.addEventListener('click', function(event) {
